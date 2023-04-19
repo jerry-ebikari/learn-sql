70f61105475f2fae98168ef63fdab3f964924ef9
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-04-19 19:54:14 +0100

    02-create-table.sql: Make id column the primary key

--- 02-create-table.sql
+++ 02-create-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE events (
-    id INT NOT NULL UNIQUE,
+    id INT PRIMARY KEY,
     name VARCHAR(300) NOT NULL CHECK (LENGTH(name) > 5),
     date_planned TIMESTAMP NOT NULL,
     image VARCHAR(300),
