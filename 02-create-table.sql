9b97d2ac0e87d63a404636d4359d4099c4ab0371
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-04-19 15:28:57 +0100

    02-create-table.sql: Make description column NOT NULL

--- 02-create-table.sql
+++ 02-create-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE events (
     name VARCHAR(300) NOT NULL,
     date_planned TIMESTAMP NOT NULL,
     image VARCHAR(300),
-    description TEXT,
+    description TEXT NOT NULL,
     max_participants INT CHECK (max_participants > 0),
     min_age INT CHECK (min_age > 0)
 );
