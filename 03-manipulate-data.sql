08527e698d7a744f3f2333be492a9b6ab29e21c1
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-04-19 20:18:51 +0100

    03-manipulate-data.sql: Added second set of values

--- 03-manipulate-data.sql
+++ 03-manipulate-data.sql
@@ -10,4 +10,10 @@ INSERT INTO events (
     'This is the first of a series of awesome events',
     20,
     18
+), (
+    'Event number two',
+    '2023-01-10 18:30:00',
+    'First one was awesome, this is the second, get ready!',
+    30,
+    18
 );
